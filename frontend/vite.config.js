02c6cf8b045dfaefffe7a1149b9247f6e8130b1a
tanuj <tanujbhandari10@gmail.com>
2025-05-19 01:24:10 +0530

    change in vite.congig.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,"./src"),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist", // ✅ Add this line
+  },
+});
+
